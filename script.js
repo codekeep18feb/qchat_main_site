231860e58ae554b5e89ff7a7934864bfb95c2a6b
Reetu Singh <codewithreetu@gmail.com>
2024-09-25 16:54:16 +0530

    fixed css for the mobile view

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const peerAdmin = document.querySelector(".peer_list .toggle");
 const sublist = document.querySelector(".peer_list_sublist");
 const worldChat = document.querySelector(".world-chat");
-const v1_case = document.querySelector(".v1_case");
+const v1_case = document.getElementById("v1");
+const v2_case = document.getElementById("v2");
+const v3_case = document.getElementById("v3");
 const navbar = document.querySelector('.navbar');
 const utilityOptions = document.querySelector(".utility-options");
 const worldChatWrapper = document.querySelector(".world-chat-wrapper");
@@ -94,6 +96,139 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  v2_case.addEventListener("click", () => {
+
+    // • if they don't have an authentication system in product but want it one with chat 
+    // • if they don't have an authentication system in product and they dont want one either but chat
+
+    // worldChat.classList.add('v1Case');
+
+    worldChat.innerHTML = `
+      <section class="v1_wrapper">
+        <h4>CASE1 - If they already have an authentication system in product (JWT) and just want the chat </h4>
+        <div class="v1_contants">
+          <div class="v1_contant_cards">
+            <h5>Benefits</h5>
+            <ul>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <img class="icon" src="Asset/arrow.png" alt="icon" />
+              </ul>
+              <button class="read_more_btn">Read more</button>
+              </div>
+          <div class="v1_contant_cards">
+            <h5>Provides</h5>
+            <ul>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <img class="icon" src="Asset/arrow.png" alt="icon" />
+            </ul>
+            <button class="read_more_btn">Read more</button>
+          </div>
+          <div class="v1_contant_cards">
+            <h5>Steps</h5>
+            <ul>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <img class="icon" src="Asset/arrow.png" alt="icon" />
+            </ul>
+            <button class="read_more_btn">Read more</button>
+          </div>
+          <div class="v1_contant_cards">
+            <h5>Examples</h5>
+            <ul>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <img class="icon" src="Asset/arrow.png" alt="icon" />
+            </ul>
+            <button class="read_more_btn">Read more</button>
+          </div>
+        </div>
+      </section>
+    `;
+
+    if (window.innerWidth <= 768) {
+      utilityOptions.style.display = "none";
+      worldChatWrapper.style.display = "block";
+    } else {
+      worldChatWrapper.style.display = "block";
+    }
+  });
+
+  v3_case.addEventListener("click", () => {
+
+    // • if they don't have an authentication system in product but want it one with chat 
+    // • if they don't have an authentication system in product and they dont want one either but chat
+
+    // worldChat.classList.add('v1Case');
+
+    worldChat.innerHTML = `
+      <section class="v1_wrapper">
+        <h4>CASE1 - If they already have an authentication system in product (JWT) and just want the chat </h4>
+        <div class="v1_contants">
+          <div class="v1_contant_cards">
+            <h5>Benefits</h5>
+            <ul>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <img class="icon" src="Asset/arrow.png" alt="icon" />
+              </ul>
+              <button class="read_more_btn">Read more</button>
+              </div>
+          <div class="v1_contant_cards">
+            <h5>Provides</h5>
+            <ul>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <img class="icon" src="Asset/arrow.png" alt="icon" />
+            </ul>
+            <button class="read_more_btn">Read more</button>
+          </div>
+          <div class="v1_contant_cards">
+            <h5>Steps</h5>
+            <ul>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <img class="icon" src="Asset/arrow.png" alt="icon" />
+            </ul>
+            <button class="read_more_btn">Read more</button>
+          </div>
+          <div class="v1_contant_cards">
+            <h5>Examples</h5>
+            <ul>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <li>Lorem Ipsum is simply dummy text of the printing</li>
+              <img class="icon" src="Asset/arrow.png" alt="icon" />
+            </ul>
+            <button class="read_more_btn">Read more</button>
+          </div>
+        </div>
+      </section>
+    `;
+
+    if (window.innerWidth <= 768) {
+      utilityOptions.style.display = "none";
+      worldChatWrapper.style.display = "block";
+    } else {
+      worldChatWrapper.style.display = "block";
+    }
+  });
   backButton.addEventListener("click", () => {
     if (window.innerWidth <= 768) {
       worldChatWrapper.style.display = "none";
