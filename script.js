4e3aacb8a3d8e5c8f874a6fade6cfc527d8c255c
Reetu Singh <codewithreetu@gmail.com>
2024-09-26 12:07:31 +0530

    made function for the V1,V2,V3 case

--- script.js
+++ script.js
@@ -6,18 +6,22 @@ const v2_case = document.getElementById("v2");
 const v3_case = document.getElementById("v3");
 const navbar = document.querySelector('.navbar');
 const utilityOptions = document.querySelector(".utility-options");
-const worldChatWrapper = document.querySelector(".world-chat-wrapper");
+const chatWrapper = document.querySelector(".chat-type-wrapper");
 const backButton = document.querySelector(".back-btn button");
+const navLinks = document.getElementById("nav-links");
+
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
   peerAdmin.addEventListener("click", () => {
     if (window.innerWidth <= 768) {
-      if (worldChatWrapper.style.display === "block") {
-        worldChatWrapper.style.display = "none";
+      if (chatWrapper.style.display === "block") {
+        chatWrapper.style.display = "none";
         utilityOptions.style.display = "block";
       } else {
         utilityOptions.style.display = "none";
-        worldChatWrapper.style.display = "block";
+        chatWrapper.style.display = "block";
       }
     } else {
       if (sublist.style.display === "block") {
@@ -36,210 +40,115 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // worldChat.classList.add('v1Case');
 
-    worldChat.innerHTML = `
-      <section class="v1_wrapper">
-        <h4>CASE1 - If they already have an authentication system in product (JWT) and just want the chat </h4>
-        <div class="v1_contants">
-          <div class="v1_contant_cards">
-            <h5>Benefits</h5>
-            <ul>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <img class="icon" src="Asset/arrow.png" alt="icon" />
-              </ul>
-              <button class="read_more_btn">Read more</button>
-              </div>
-          <div class="v1_contant_cards">
-            <h5>Provides</h5>
-            <ul>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <img class="icon" src="Asset/arrow.png" alt="icon" />
-            </ul>
-            <button class="read_more_btn">Read more</button>
-          </div>
-          <div class="v1_contant_cards">
-            <h5>Steps</h5>
-            <ul>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <img class="icon" src="Asset/arrow.png" alt="icon" />
-            </ul>
-            <button class="read_more_btn">Read more</button>
-          </div>
-          <div class="v1_contant_cards">
-            <h5>Examples</h5>
-            <ul>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <img class="icon" src="Asset/arrow.png" alt="icon" />
-            </ul>
-            <button class="read_more_btn">Read more</button>
-          </div>
-        </div>
-      </section>
-    `;
+    const v1Content = pToAdminCaseContent(
+      "CASE1 - If they already have an authentication system in product (JWT) and just want the chat.",
+      ["Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing"],
+      ["Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing"],
+      ["Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing"],
+      ["Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing"],
 
-    if (window.innerWidth <= 768) {
-      utilityOptions.style.display = "none";
-      worldChatWrapper.style.display = "block";
-    } else {
-      worldChatWrapper.style.display = "block";
-    }
+    );
+    peerToAdminCases(v1Content);
+
   });
 
   v2_case.addEventListener("click", () => {
-
-    // • if they don't have an authentication system in product but want it one with chat 
-    // • if they don't have an authentication system in product and they dont want one either but chat
-
-    // worldChat.classList.add('v1Case');
-
-    worldChat.innerHTML = `
-      <section class="v1_wrapper">
-        <h4>CASE1 - If they already have an authentication system in product (JWT) and just want the chat </h4>
-        <div class="v1_contants">
-          <div class="v1_contant_cards">
-            <h5>Benefits</h5>
-            <ul>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <img class="icon" src="Asset/arrow.png" alt="icon" />
-              </ul>
-              <button class="read_more_btn">Read more</button>
-              </div>
-          <div class="v1_contant_cards">
-            <h5>Provides</h5>
-            <ul>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <img class="icon" src="Asset/arrow.png" alt="icon" />
-            </ul>
-            <button class="read_more_btn">Read more</button>
-          </div>
-          <div class="v1_contant_cards">
-            <h5>Steps</h5>
-            <ul>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <img class="icon" src="Asset/arrow.png" alt="icon" />
-            </ul>
-            <button class="read_more_btn">Read more</button>
-          </div>
-          <div class="v1_contant_cards">
-            <h5>Examples</h5>
-            <ul>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <img class="icon" src="Asset/arrow.png" alt="icon" />
-            </ul>
-            <button class="read_more_btn">Read more</button>
-          </div>
-        </div>
-      </section>
-    `;
-
-    if (window.innerWidth <= 768) {
-      utilityOptions.style.display = "none";
-      worldChatWrapper.style.display = "block";
-    } else {
-      worldChatWrapper.style.display = "block";
-    }
+    const v2Content = pToAdminCaseContent(
+      "CASE2 - Some other scenario with the chat",
+      ["Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing"],
+      ["Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing"],
+      ["Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing"],
+      ["Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing"],
+    );
+    peerToAdminCases(v2Content);
   });
 
   v3_case.addEventListener("click", () => {
-
-    // • if they don't have an authentication system in product but want it one with chat 
-    // • if they don't have an authentication system in product and they dont want one either but chat
-
-    // worldChat.classList.add('v1Case');
-
-    worldChat.innerHTML = `
-      <section class="v1_wrapper">
-        <h4>CASE1 - If they already have an authentication system in product (JWT) and just want the chat </h4>
-        <div class="v1_contants">
-          <div class="v1_contant_cards">
-            <h5>Benefits</h5>
-            <ul>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <img class="icon" src="Asset/arrow.png" alt="icon" />
-              </ul>
-              <button class="read_more_btn">Read more</button>
-              </div>
-          <div class="v1_contant_cards">
-            <h5>Provides</h5>
-            <ul>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <img class="icon" src="Asset/arrow.png" alt="icon" />
-            </ul>
-            <button class="read_more_btn">Read more</button>
-          </div>
-          <div class="v1_contant_cards">
-            <h5>Steps</h5>
-            <ul>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <img class="icon" src="Asset/arrow.png" alt="icon" />
-            </ul>
-            <button class="read_more_btn">Read more</button>
-          </div>
-          <div class="v1_contant_cards">
-            <h5>Examples</h5>
-            <ul>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <li>Lorem Ipsum is simply dummy text of the printing</li>
-              <img class="icon" src="Asset/arrow.png" alt="icon" />
-            </ul>
-            <button class="read_more_btn">Read more</button>
-          </div>
-        </div>
-      </section>
-    `;
-
-    if (window.innerWidth <= 768) {
-      utilityOptions.style.display = "none";
-      worldChatWrapper.style.display = "block";
-    } else {
-      worldChatWrapper.style.display = "block";
-    }
+    const v3Content = pToAdminCaseContent(
+      "CASE3 - A different use case",
+      ["Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing"],
+      ["Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing"],
+      ["Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing"],
+      ["Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing", "Lorem Ipsum is simply dummy text of the printing"],
+    );
+    peerToAdminCases(v3Content);
   });
+
+
+
   backButton.addEventListener("click", () => {
     if (window.innerWidth <= 768) {
-      worldChatWrapper.style.display = "none";
+      chatWrapper.style.display = "none";
       utilityOptions.style.display = "block";
     } else {
-      worldChatWrapper.style.display = "none";
+      chatWrapper.style.display = "none";
     }
   });
 });
+
+
 
 function toggleMenu() {
   const navLinks = document.getElementById("nav-links");
   navLinks.classList.toggle("active");
 }
+
+
+
+function pToAdminCaseContent(title, benefits, provides, steps, examples) {
+  return `
+    <section class="v1_wrapper">
+      <h4>${title}</h4>
+      <div class="v1_contants">
+        <div class="v1_contant_cards">
+          <h5>Benefits</h5>
+          <ul>${benefits.map(item => `<li>${item}</li>`).join('')}
+            <img class="icon" src="Asset/arrow.png" alt="icon" />
+          </ul>
+          <button class="read_more_btn">Read more</button>
+        </div>
+        <div class="v1_contant_cards">
+          <h5>Provides</h5>
+          <ul>${provides.map(item => `<li>${item}</li>`).join('')}
+            <img class="icon" src="Asset/arrow.png" alt="icon" />
+          </ul>
+          <button class="read_more_btn">Read more</button>
+        </div>
+        <div class="v1_contant_cards">
+          <h5>Steps</h5>
+          <ul>${steps.map(item => `<li>${item}</li>`).join('')}
+            <img class="icon" src="Asset/arrow.png" alt="icon" />
+          </ul>
+          <button class="read_more_btn">Read more</button>
+        </div>
+        <div class="v1_contant_cards">
+          <h5>Examples</h5>
+          <ul>${examples.map(item => `<li>${item}</li>`).join('')}
+            <img class="icon" src="Asset/arrow.png" alt="icon" />
+          </ul>
+          <button class="read_more_btn">Read more</button>
+        </div>
+      </div>
+    </section>
+  `;
+}
+
+function peerToAdminCases(content) {
+  console.log("content", content)
+  worldChat.innerHTML = content;
+  if (window.innerWidth <= 768) {
+    utilityOptions.style.display = "none";
+    chatWrapper.style.display = "block";
+  } else {
+    chatWrapper.style.display = "block";
+  }
+}
+
+
+document.addEventListener("click", (event) => {
+  if (!navbar.contains(event.target) && !navLinks.contains(event.target)) {
+    if (navLinks.classList.contains("active")) {
+      navLinks.classList.remove("active");
+    }
+  }
+});
